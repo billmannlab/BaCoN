16d691f76560699aa97c4ff5e7e92ceb826d7b6d
Thomas Rohde <thomasrohde@protonmail.com>
2024-06-30 22:37:14 +0200

    Removed diversity index

--- R/diversity_index.R
+++
@@ -1,6 +0,0 @@
-
-diversity_index <- \(set1, set2, .i = 0.5) {
-  if (base::length(set1) != base::length(set2)) {message("Set size is not equal!")}
-  x <- cumsum(base::sort(table(c(set1, set2)), decreasing = T))
-  max_possible <- sum(base::length(set1), base::length(set2)) * .i
-  return(base::length(x[which(x <= max_possible)]) / max_possible)}
